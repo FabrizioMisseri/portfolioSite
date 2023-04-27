8677b548d51198f97e8cb578f217cb4c7e3feb3d
FabrizioMisseri <misserifabrizio@gmail.com>
2023-04-27 10:55:27 +0200

    scaffolding

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,26 @@
+<script>
+import { store } from '../store';
+
+export default {
+    name: 'HomePage',
+
+    data() {
+        return {
+            store,
+
+        }
+    },
+
+    created() {
+
+    },
+
+    methods: {
+
+    }
+}
+</script>
+
+<template></template>
+
+<style lang="scss" scoped></style>
---
+++ src/pages/SecondPage.vue
@@ -0,0 +1,21 @@
+<script>
+import { store } from '../store';
+
+export default {
+    name: 'SecondPage',
+
+    data() {
+        return {
+            store,
+        }
+    },
+
+    methods: {
+
+    }
+}
+</script>
+
+<template></template>
+
+<style lang="scss" scoped></style>
