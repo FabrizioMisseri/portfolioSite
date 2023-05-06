365e7e40fd1eebf88a2468c57d1fa3ed46549573
FabrizioMisseri <misserifabrizio@gmail.com>
2023-05-06 11:22:52 +0200

    create singleProject && edit-storeJS

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,17 @@
+<script>
+
+export default {
+
+    name: 'SingleProject',
+
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+<template></template>
+
+<style lang="scss" scoped></style>
--- src/store.js
+++ src/store.js
@@ -12,4 +12,28 @@ export const store = reactive({
         "fotoalbum/2.png",
         "fotoalbum/3.png",
     ],
+
+    // -----------------------------------------------------
+
+    objectsArray: [
+        {
+            title: 'BoolKing',
+            photos: [
+                "/assets/img/boolking/2.png",
+                "/assets/img/boolking/1.png",
+            ],
+            video: '/assets/video/boolking/',
+            description: 'lorem ipsum ...',
+        },
+        {
+            title: 'FotoAlbum',
+            photos: [
+                "/assets/img/fotoalbum/1.png",
+                "/assets/img/fotoalbum/2.png",
+                "/assets/img/fotoalbum/3.png",
+            ],
+            video: '/assets/video/fotoalbum/',
+            description: 'lorem ipsum ...',
+        },
+    ],
 });
