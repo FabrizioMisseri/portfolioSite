1127a4e7b34345bf57b6c7729982e9693ea61f7c
FabrizioMisseri <misserifabrizio@gmail.com>
2023-05-06 11:52:34 +0200

    add props in Projects && edit-SingleProject

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,17 +1,91 @@
 <script>
+// import slider
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+import { Pagination } from 'swiper';
+import 'swiper/css/pagination';
+// -------------
 
 export default {
 
+    // setup slider
+    setup() {
+        const onSwiper = (swiper) => {
+            console.log(swiper);
+        };
+        const onSlideChange = () => {
+            console.log('slide change');
+        };
+        return {
+            onSwiper,
+            onSlideChange,
+        };
+    },
+    // -------------
+
     name: 'SingleProject',
+
+    components: {
+        Swiper,
+        SwiperSlide,
+        modules: [Pagination],
+    },
+
+    props: {
+        project: Object,
+    },
 
     data() {
         return {
 
         }
     },
+
+    methods: {
+        getImage(path) {
+            return new URL(path, import.meta.url).href;
+        }
+    }
 }
 </script>
 
-<template></template>
+<template>
+    <section class="py-5">
+        <!-- TITLE -->
+        <h2 class="mb-3 text-center"> {{ project.title }} </h2>
+        <!-- / TITLE -->
 
-<style lang="scss" scoped></style>
+        <!-- SLIDER -->
+        <swiper class="m-4" :modules="modules" :slides-per-view="1" :space-between="0" :pagination="true" @swiper="onSwiper"
+            @slideChange="onSlideChange" :loop="true">
+            <!-- all photos -->
+            <swiper-slide class="slider" v-for="(photo, index) in project.photos" :key="index">
+                <img :src="getImage(photo)" alt="">
+            </swiper-slide>
+            <!-- / all photos -->
+        </swiper>
+        <!-- / SLIDER -->
+
+        <!-- TEXT -->
+        <h5>Descrizione progetto:</h5>
+        <p>
+            {{ project.description }}
+        </p>
+        <!-- / TEXT -->
+    </section>
+</template>
+
+<style lang="scss" scoped>
+section {
+    max-width: 90%;
+    margin: 0 auto;
+
+    .slider {
+        text-align: center;
+
+        img {
+            max-width: 85%;
+        }
+    }
+}
+</style>
--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,35 +1,15 @@
 <script>
 import { store } from '../store';
 import Navbar from '../components/Navbar.vue';
-// import slider
-import { Swiper, SwiperSlide } from 'swiper/vue';
-import 'swiper/css';
-import { Pagination } from 'swiper';
-import 'swiper/css/pagination';
-// -------------
+import SingleProject from '../components/SingleProject.vue';
+
 
 export default {
-    // setup slider
-    setup() {
-        const onSwiper = (swiper) => {
-            console.log(swiper);
-        };
-        const onSlideChange = () => {
-            console.log('slide change');
-        };
-        return {
-            onSwiper,
-            onSlideChange,
-        };
-    },
-    // -------------
 
     name: 'Projects',
     components: {
         Navbar,
-        Swiper,
-        SwiperSlide,
-        modules: [Pagination],
+        SingleProject,
     },
 
     data() {
@@ -37,41 +17,19 @@ export default {
             store,
         }
     },
-
-    methods: {
-        getImage(path) {
-            return new URL(this.store.basePathString + path, import.meta.url).href;
-        }
-    }
 }
 </script>
 
 <template>
     <Navbar></Navbar>
 
-    <!-- debug -->
-    <h2 class="py-5">WORK IN PROGRESS ...</h2>
-    <!-- / debug -->
-
     <!-- body wrapper -->
     <section class="container">
-        <!-- SLIDER -->
-        <swiper :modules="modules" :slides-per-view="1" :space-between="0" :pagination="true" @swiper="onSwiper"
-            @slideChange="onSlideChange" :loop="true">
 
-            <!-- all photos debug -->
-            <swiper-slide v-for="(photo, index) in store.photos" :key="index">
-                <img :src="getImage(photo)" alt="">
-            </swiper-slide>
-            <!-- / all photos debug -->
-
-        </swiper>
-        <!-- / SLIDER -->
+        <SingleProject v-for="(project, index) in store.objectsArray" :project="project"></SingleProject>
 
     </section>
     <!-- / body wrapper -->
 </template>
 
-<style lang="scss" scoped>
-@use "../styles/partials/variables" as *;
-</style>
+<style lang="scss" scoped></style>
