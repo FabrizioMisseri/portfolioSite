ea22b2bdc072282cb131fda0d6d1112042503c13
FabrizioMisseri <misserifabrizio@gmail.com>
2023-05-08 17:57:28 +0200

    add about-me content

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -46,6 +46,19 @@ export default {
             </div>
             <!-- /SKILLS -->
 
+            <!-- ABOUT-ME -->
+            <div class="py-5">
+                <h4 class="title mb-3">About-me:</h4>
+                <p>
+                    Da sempre affascinato dal mondo della programmazione ho trovato nella scrittura del codice l'ambiente
+                    ideale
+                    dove convergere sete di conoscenza, esperienza e curiosità. Un mondo stimolante ed in costante
+                    evoluzione,
+                    dove sento di poter esprimere tutta la mia creatività.
+                </p>
+            </div>
+            <!-- / ABOUT-ME -->
+
         </div>
 
     </section>
